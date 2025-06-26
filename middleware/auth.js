76cc57366e3716594813b931e61cdb91bb09ec8a
Sachintha Madushan <49426927+sachinthamadushan@users.noreply.github.com>
2025-06-26 12:06:59 +0530

    Bug fixed auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,11 @@ const jwt = require('jsonwebtoken');
 const auth = async (req, res, next) => {
     try {
       const authHeader = req.headers.authorization;
-      console.log(authHeader);
-      console.log("Token in storage:", localStorage.getItem('token'));
+      console.log("AUTH HEADER:", authHeader);
       
       if(!authHeader) return res.status(401)
         .json({ error: 'No token provided' });
-    const token = authHeader;
+    const token = authHeader.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
